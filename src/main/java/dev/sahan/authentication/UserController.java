f476f4407251f8e1a305b75cfc6fd013ed47a8b4
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-10 00:53:16 +0530

    created controller class

---
+++ src/main/java/dev/sahan/authentication/UserController.java
@@ -0,0 +1,95 @@
+package dev.sahan.authentication;
+
+import dev.sahan.authentication.User;
+import dev.sahan.authentication.UserRepository;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        // Check if the user already exists in the database
+        if (userRepository.existsByUsername(user.getUsername())) {
+            throw new RuntimeException("Username already exists");
+        }
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        // Save the user to the database
+        User savedUser = userRepository.save(user);
+
+        // Return the saved user
+        return savedUser;
+    }
+
+    @PostMapping("/login")
+    public User loginUser(@RequestBody User user) {
+        // Find the user by username and password
+        User existingUser = userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+
+        // Check if the user exists and the password is correct
+        if (existingUser == null) {
+            throw new RuntimeException("Invalid username or password");
+        }
+
+        // Return the authenticated user
+        return existingUser;
+    }
+
+    @PutMapping("/edit/{id}")
+    public User editUser(@PathVariable("id") String id, @RequestBody User user) {
+        // Find the user by id
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        // Check if the user exists
+        if (!optionalUser.isPresent()) {
+            throw new RuntimeException("User not found");
+        }
+
+        // Update the user details
+        User existingUser = optionalUser.get();
+        existingUser.setUsername(user.getUsername());
+        existingUser.setPassword(user.getPassword());
+        existingUser.setEmail(user.getEmail());
+
+        // Save the updated user to the database
+        User updatedUser = userRepository.save(existingUser);
+
+        // Return the updated user
+        return updatedUser;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUser(@PathVariable("id") String id) {
+        // Find the user by id
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        // Check if the user exists
+        if (!optionalUser.isPresent()) {
+            throw new RuntimeException("User not found");
+        }
+
+        // Delete the user from the database
+        userRepository.delete(optionalUser.get());
+    }
+}
+
